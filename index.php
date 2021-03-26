c17e803e2dacb8aae3c201aea5f1f02fb729d59a
Yuri D Dubler <YuriDubler@Yuris-MacBook-Pro.local>
2021-03-26 17:12:26 -0600

    Updates

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
     <h1>PHP Refresher</h1>
-    <div>
+    <div id="conditional">
     <h3><u>Conditional</u></h3>
         <pre>
 $x=1;
@@ -32,7 +32,7 @@ else
         }
         ?>
     </div>
-    <div>
+    <div id="listing-array">
     <h3><u>Listing Contents of Array</u></h3>
         <pre>
 $people = array("First Entry in Array","Second Entry in Array", "Third Entry", "Fourth");
@@ -50,13 +50,57 @@ foreach($people as $person)
         <?php
         // List contents of array
         $people = array("First Entry in Array","Second Entry in Array", "Third Entry", "Fourth");
-        $length = count($people);
 
         foreach($people as $person)
         {
             echo $person;
             echo "<br/>";
         }
+        ?>
+    </div>
+    <div id="sum-array">
+    <h3><u>Sum Contents of Array</u></h3>
+        <pre>
+$numbers = array(1,2,3,4,5);
+$count = count($numbers);
+$sum = 0;
+
+for($i=0;$i<$count;$i++)
+{
+    if($i < $count-1){
+        echo " $numbers[$i] + ";
+    }
+    else
+    {
+        echo " $numbers[$i] = ";
+    }
+
+    $sum += $numbers[$i];
+}
+echo "$sum"
+        </pre>
+        RESULT:
+        <br/>
+        <br/>
+        <?php
+        // Sum contents of array
+        $numbers = array(1,2,3,4,5);
+        $count = count($numbers);
+        $sum = 0;
+
+        for($i=0;$i<$count;$i++)
+        {
+            if($i < $count-1){
+                echo " $numbers[$i] + ";
+            }
+            else
+            {
+                echo " $numbers[$i] = ";
+            }
+
+            $sum += $numbers[$i];
+        }
+        echo "$sum"
         ?>
     </div>
 </body>
