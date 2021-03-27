0aa161d10393254c8993a171e5b6b0b7f9f0d07b
Yuri D Dubler <YuriDubler@Yuris-MacBook-Pro.local>
2021-03-27 10:17:50 -0600

    Formatted HTML document

--- index.php
+++ index.php
@@ -15,8 +15,9 @@ else
     echo"$x==1 is not true.";
 }
         </pre>
-        RESULT:
-        <br/>
+        <hr style="margin-top:0px">
+        RESULT
+        <hr style="margin-bottom:0px">
         <br/>
         <?php
         // Basic Conditional
@@ -44,8 +45,9 @@ foreach($people as $person)
     echo "&lt;br/&gt;";
 }
         </pre>
-        RESULT:
-        <br/>
+        <hr style="margin-top:0px">
+        RESULT
+        <hr style="margin-bottom:0px">
         <br/>
         <?php
         // List contents of array
@@ -79,8 +81,9 @@ for($i=0;$i<$count;$i++)
 }
 echo "$sum"
         </pre>
-        RESULT:
-        <br/>
+        <hr style="margin-top:0px">
+        RESULT
+        <hr style="margin-bottom:0px">
         <br/>
         <?php
         // Sum contents of array
@@ -101,6 +104,78 @@ echo "$sum"
             $sum += $numbers[$i];
         }
         echo "$sum"
+        ?>
+    </div>
+    <div id="variable-types" style="margin:20px;padding:20px;border:1px solid black;border-radius:20px">
+    <h3 style="margin-top:0px"><u>Conditional</u></h3>
+        <pre>
+$int = 1234;
+$float = 2.2;
+$bool = true;
+$string = "a string of characters";
+define("NAME_OF_CONSTANT", "This is a constant!");
+
+echo "\$int = $int;&lt;br/&gt;";
+echo "is integer? " . is_int($int) . "&lt;br/&gt;";
+echo var_dump($int) . "&lt;br/&gt;";
+echo "&lt;br/&gt;";
+
+echo "\$float = $float;&lt;br/&gt;";
+echo "is float? " . is_float($float) . "&lt;br/&gt;";
+echo var_dump($float) . "&lt;br/&gt;";
+echo "&lt;br/&gt;";
+
+echo "\$bool = $bool;&lt;br/&gt;";
+echo "is bool? " . is_bool($bool) . "&lt;br/&gt;";
+echo var_dump($bool) . "&lt;br/&gt;";
+echo "&lt;br/&gt;";
+
+echo '$string = "a string of characters"&lt;br/&gt;';
+echo "is string? " . is_string($string) . "&lt;br/&gt;";
+echo var_dump($string) . "&lt;br/&gt;";
+echo "&lt;br/&gt;";
+
+echo "NAME_OF_CONSTANT = NAME_OF_CONSTANT;&lt;br/&gt;";
+echo "is string? " . is_string(NAME_OF_CONSTANT) . "&lt;br/&gt;";
+echo var_dump(NAME_OF_CONSTANT) . "&lt;br/&gt;";
+echo "&lt;br/&gt;";
+        </pre>
+        <hr style="margin-top:0px">
+        RESULT
+        <hr style="margin-bottom:0px">
+        <br/>
+        <?php
+        // Basic Conditional
+        $int = 1234;
+        $float = 2.2;
+        $bool = true;
+        $string = "a string of characters";
+        define("NAME_OF_CONSTANT", "This is a constant!");
+        
+        echo "\$int = $int;<br/>";
+        echo "is integer? " . is_int($int) . "<br/>";
+        echo var_dump($int) . "<br/>";
+        echo "<br/>";
+
+        echo "\$float = $float;<br/>";
+        echo "is float? " . is_float($float) . "<br/>";
+        echo var_dump($float) . "<br/>";
+        echo "<br/>";
+
+        echo "\$bool = $bool;<br/>";
+        echo "is bool? " . is_bool($bool) . "<br/>";
+        echo var_dump($bool) . "<br/>";
+        echo "<br/>";
+        
+        echo '$string = "a string of characters"<br/>';
+        echo "is string? " . is_string($string) . "<br/>";
+        echo var_dump($string) . "<br/>";
+        echo "<br/>";
+
+        echo 'NAME_OF_CONSTANT = "' . NAME_OF_CONSTANT . '"<br/>';
+        echo "is string? " . is_string(NAME_OF_CONSTANT) . "<br/>";
+        echo var_dump(NAME_OF_CONSTANT) . "<br/>";
+        echo "<br/>";
         ?>
     </div>
 </body>
